3b66126a8463efdc090c9b68e966e817738972dd
Anurag Gupta (CW) <Anurag.Gupta@jda.com>
2020-02-09 22:23:05 +0530

    Word Definition Done.

--- services/basic/definitions.js
+++ services/basic/definitions.js
@@ -3,6 +3,25 @@
  * @description Display definitions of a given word.
  */
 
-module.exports = function () {
-    
+const api = require('./../../api');
+
+module.exports = function (word) {
+    api({ api: 'defn', word: word }).then(
+        data => {
+            if (Array.isArray(data)) {
+                console.log(`Found ${data.length} definitions of ${word}: `);
+                console.log('=============================================\n')
+                data.forEach(function (elem, index) {
+                    console.log(`${index + 1}.`, elem.text);
+                })
+
+                console.log('\n\n');
+            }
+        }
+    ).catch(
+        err => {
+            console.log('Found error:: ', err.message);
+            console.log('Please try again.');
+        }
+    )
 }
