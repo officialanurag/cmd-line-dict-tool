f40f1b9656b87403f62576e7a28dde1317fc49b9
Anurag Gupta (CW) <Anurag.Gupta@jda.com>
2020-02-09 22:31:51 +0530

    Word Antonyms & Synonyms Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 const definition = require('./services/basic/definitions');
+const synonyms = require('./services/basic/synonyms');
+const antonyms = require('./services/basic/antonyms');
+
--- services/basic/synonyms.js
+++ services/basic/synonyms.js
@@ -3,6 +3,29 @@
  * @description Display synonyms of a given word.
  */
 
-module.exports = function () {
-    
+const api = require('./../../api');
+
+module.exports = function (word) {
+    api({ api: 'syn', word: word }).then(
+        data => {
+            if (Array.isArray(data)) {
+                data.forEach((elem) => {
+                    if (elem.relationshipType == 'synonym') {
+                        console.log(`Found ${elem.words.length} synonyms of ${word}: `);
+                        console.log('=============================================\n')
+                        elem.words.forEach((elem, index) => {
+                            console.log(`${index + 1}.`, elem);
+                        });
+                    }
+                });
+
+                console.log('\n\n');
+            }
+        }
+    ).catch(
+        err => {
+            console.log('Found error:: ', err.message);
+            console.log('Please try again.');
+        }
+    );
 }
