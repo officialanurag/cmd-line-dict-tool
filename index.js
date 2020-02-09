3b6eb73d177704abd8dfc3df2442b640b429a708
officialanurag <Anurag.Gupta@jda.com>
2020-02-09 22:45:56 +0530

    Word Dict Done.

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const definition = require('./services/basic/definitions');
 const synonyms = require('./services/basic/synonyms');
 const antonyms = require('./services/basic/antonyms');
 const examples = require('./services/basic/examples');
+const wordDict = require('./services/extended/word-dict');
--- services/extended/word-dict.js
+++ services/extended/word-dict.js
@@ -3,6 +3,14 @@
  * @description Display Word Definitions, Word Synonyms, Word Antonyms & Word Examples for a given word.
  */
 
- module.exports = function () {
-     
- }
+const definitions = require('./../basic/definitions');
+const synonyms = require('./../basic/synonyms');
+const antonyms = require('./../basic/antonyms');
+const examples = require('./../basic/examples');
+
+module.exports = function (word) {
+    definitions(word);
+    synonyms(word);
+    antonyms(word);
+    examples(word);
+}
